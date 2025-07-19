9a99b74d78b3b652bcecb5368bcc2a510f22c726
Sami <samiuddin12318@gmail.com>
2025-07-20 02:23:05 +0500

    controller

---
+++ StudentLoginSystem/Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using StudentLoginSystem.Models;
+using StudentLoginSystem.Services;
+using System.Diagnostics.Eventing.Reader;
+
+namespace StudentLoginSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly GoogleSheetService _sheetService;
+
+        public AccountController(GoogleSheetService sheetService)
+        {
+            _sheetService = sheetService;
+        }
+
+        [HttpGet]
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model, string action)
+        {
+            if (action == "login")
+            {
+                var users = await _sheetService.GetUsersAsync();
+
+                var user = users.FirstOrDefault(u =>
+                    u.RollNumber == model.Username && u.Password == model.Password);
+
+                if (user == null)
+                {
+                    ViewBag.Error = "Invalid credentials";
+                    return View(model);
+                }
+
+                HttpContext.Session.SetString("User", JsonConvert.SerializeObject(user));
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else if (action == "getPassword")
+            {
+                var users = _sheetService.GetUsersAsync().Result;
+                var user = users.FirstOrDefault(u => u.RollNumber == model.Username);
+                if (user != null)
+                {
+                    ViewBag.Password = user.Password;
+                }
+                else
+                {
+                    ViewBag.Error = "Roll number not found.";
+                }
+
+                return View(model);
+            }
+            return View(model);
+
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult GetPassword(LoginViewModel model)
+        {
+            var users = _sheetService.GetUsersAsync().Result;
+
+            var user = users.FirstOrDefault(u => u.RollNumber == model.Username);
+            if (user != null)
+            {
+                ViewBag.Password = user.ProfileGrNo;
+            }
+            else
+            {
+                ViewBag.Error = "Roll number not found.";
+            }
+
+            return View("Login", model); // Re-render login view with result
+        }
+
+    }
+}
---
+++ StudentLoginSystem/Controllers/DashboardController .cs
@@ -0,0 +1,30 @@
+﻿using Google.Apis.Drive.v3;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using StudentLoginSystem.Models;
+using StudentLoginSystem.Services;
+
+namespace StudentLoginSystem.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly GoogleDriveService _driveService;
+
+        public DashboardController(GoogleDriveService driveService)
+        {
+            _driveService = driveService;
+        }
+        public IActionResult Index()
+        {
+            var userJson = HttpContext.Session.GetString("User");
+            if (string.IsNullOrEmpty(userJson)) return RedirectToAction("Login", "Account");
+
+            var user = JsonConvert.DeserializeObject<UserModel>(userJson);
+
+            // Set voucher link
+            user.VoucherUrl = _driveService.GetVoucherUrlByRollNumber(user.RollNumber);
+
+            return View(user);
+        }
+    }
+}
