1c315616454da98614d281cacb5132fb30781661
Sami <samiuddin12318@gmail.com>
2025-07-20 02:23:58 +0500

    model and views

---
+++ StudentLoginSystem/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentLoginSystem.Models
+{
+    public class UserModel
+    {
+        public string RollNumber { get; set; }
+        public string StudentName { get; set; }
+        public string FatherName { get; set; }
+        public string Program { get; set; }
+        public string ProfileGrNo { get; set; }
+        public string Password { get; set; }
+        public string Group { get; set; }
+        public string StandardClass { get; set; }
+        public string VoucherUrl { get; set; }
+
+    }
+}
