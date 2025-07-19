23a8c9f2672efa48f213886803b8fad4873ec88b
Sami <samiuddin12318@gmail.com>
2025-07-20 02:24:19 +0500

    services

---
+++ StudentLoginSystem/Services/GoogleDriveService.cs
@@ -0,0 +1,42 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+
+namespace StudentLoginSystem.Services
+{
+    public class GoogleDriveService
+    {
+        private readonly DriveService _driveService;
+        private readonly string folderId = "1FpHhFY_6WwQ3p0KuWCgQ2jcr01NUjsv5";
+
+        public GoogleDriveService()
+        {
+            var credential = GoogleCredential
+                .FromFile(@"F:\MamaParsi\StudentLoginSystem\StudentLoginSystem\Files\studentlogin-465315-ff341c2155e6.json") // Path to your service account file
+                .CreateScoped(DriveService.Scope.DriveReadonly);
+
+            _driveService = new DriveService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "StudentLoginSystem"
+            });
+        }
+
+        public string GetVoucherUrlByRollNumber(string rollNumber)
+        {
+            var request = _driveService.Files.List();
+            request.Q = $"name = '{rollNumber}.pdf' and '{folderId}' in parents";
+            request.Fields = "files(id, name)";
+            var result = request.Execute();
+
+            var file = result.Files.FirstOrDefault();
+            if (file != null)
+            {
+                return $"https://drive.google.com/file/d/{file.Id}/view";
+            }
+
+            return null;
+        }
+    }
+
+}
---
+++ StudentLoginSystem/Services/GoogleSheetService.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using StudentLoginSystem.Models;
+
+namespace StudentLoginSystem.Services
+{
+    public class GoogleSheetService
+    {
+
+        private const string CsvUrl = "https://docs.google.com/spreadsheets/d/1he_O73c-kr8jyjLxJRA3f3kTij79WgEXungoUUPuWGQ/export?format=csv&gid=0";
+
+        public async Task<List<UserModel>> GetUsersAsync()
+        {
+            var users = new List<UserModel>();
+
+            using var httpClient = new HttpClient();
+            var csvData = await httpClient.GetStringAsync(CsvUrl);
+
+            var lines = csvData.Split('\n');
+            for (int i = 1; i < lines.Length; i++) // Skip header
+            {
+                var columns = lines[i].Split(',');
+
+                if (columns.Length < 8) continue;
+
+                users.Add(new UserModel
+                {
+                    RollNumber = columns[0].Trim(),
+                    StudentName = columns[1].Trim(),
+                    FatherName = columns[2].Trim(),
+                    Program = columns[3].Trim(),
+                    Group = columns[4].Trim(),
+                    StandardClass = columns[5].Trim(),
+                    Password = columns[8].Trim()
+                });
+            }
+
+            return users;
+        }
+    }
+}
